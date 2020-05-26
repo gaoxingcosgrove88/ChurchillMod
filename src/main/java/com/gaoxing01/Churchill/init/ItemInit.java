13693a69d6fab9f5677144e29aa2407aa48a6588
gaoxingcosgrove88@gmail.com <gaoxingcosgrove88@gmail.com>
2020-05-26 13:38:17 -0400

    Initial Commit, before custom item

--- src/main/java/com/gaoxing01/Churchill/init/ItemInit.java
+++ src/main/java/com/gaoxing01/Churchill/init/ItemInit.java
@@ -1,4 +1,23 @@
 package com.gaoxing01.Churchill.init;
 
+import com.gaoxing01.Churchill.churchill;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid = churchill.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+//makes class object holder, object holder of your mod(MOD_ID)
+@ObjectHolder(churchill.MOD_ID)
 public class ItemInit {
+
+    public static final Item example_item = null;
+
+    @SubscribeEvent
+    public static void registerItems(final RegistryEvent.Register<Item> event) {
+
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("example item"));
+    }
 }
